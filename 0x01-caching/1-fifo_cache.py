627e20b46928412dcdf99646330ee25c1899d22a
maranatha <maranatha@ubuntu.myguest.virtualbox.org>
2023-08-05 11:43:09 +0100

    Hot fix

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,10 +1,31 @@
+#!/usr/bin/env python3
+"""FIFOCache Module"""
+
 from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
+    """
+    FIFOCache class that inherits from BaseCaching and implements a caching system
+    using the FIFO (First-In-First-Out) algorithm.
+    """
+
     def __init__(self):
+        """
+        Initialize the FIFOCache instance.
+        """
         super().__init__()
 
     def put(self, key, item):
+        """
+        Add an item to the cache using the FIFO algorithm.
+
+        Args:
+            key: The key to be added.
+            item: The item to be added.
+
+        Returns:
+            None
+        """
         if key is None or item is None:
             return
 
@@ -18,6 +39,16 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
+        """
+        Retrieve the value associated with the given key from the cache.
+
+        Args:
+            key: The key whose value is to be retrieved.
+
+        Returns:
+            The value associated with the key, or None if key is None
+            or not present in the cache.
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
